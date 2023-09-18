426a71109aca36ec1a0258c4a5cb276b60923d30
raffnhd <raffnhd@gmail.com>
2023-09-18 11:33:03 +0200

    test

--- src/ordination/DagligSkaev.java
+++ src/ordination/DagligSkaev.java
@@ -2,6 +2,7 @@ package ordination;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 
 public class DagligSkaev extends Ordination{
@@ -26,7 +27,18 @@ public class DagligSkaev extends Ordination{
 
     @Override
     public double doegnDosis() {
-        return 0;
+        int antalDage = (int) ChronoUnit.DAYS.between(getStartDen(), getSlutDen());
+
+        if (antalDage == 0) {
+            return 0.0;
+        }
+        else {
+            double totalDosis = 0.0;
+            for (Dosis d : dosis) {
+                totalDosis += d.getAntal();
+            }
+            return totalDosis / antalDage;
+        }
     }
 
     @Override
